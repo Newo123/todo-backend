016fb83da4dcdb06fff73726225756ee3dffaa41
Хижа Кирилл <scerep529@gmail.com>
2026-08-18 01:01:25 +0600

    create zap logger adapter for abstaction logger

---
+++ internal/infrastructure/logger/zap/logger.go
@@ -0,0 +1,94 @@
+package zap
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/Newo123/todo-backend/internal/infrastructure/logger"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+type Logger struct {
+	*zap.Logger
+	file *os.File
+}
+
+func NewLogger(config Config) (*Logger, error) {
+	zapLvl := zap.NewAtomicLevel()
+	if err := zapLvl.UnmarshalText([]byte(config.Level)); err != nil {
+		return nil, fmt.Errorf("unmarshal log level: %w", err)
+	}
+
+	if err := os.MkdirAll(config.Folder, 0755); err != nil {
+		return nil, fmt.Errorf("mkdir log folder: %w", err)
+	}
+
+	timestamp := time.Now().UTC().Format("2006-01-02T15-04-05.000000")
+	logFilePath := filepath.Join(
+		config.Folder,
+		fmt.Sprintf("%s.log", timestamp),
+	)
+
+	logFile, err := os.OpenFile(logFilePath, os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return nil, fmt.Errorf("open log file: %w", err)
+	}
+
+	zapConfig := zap.NewDevelopmentEncoderConfig()
+	zapConfig.EncodeTime = zapcore.TimeEncoderOfLayout("2006-01-02T15:04:05.000000")
+
+	zapEncoder := zapcore.NewConsoleEncoder(zapConfig)
+
+	core := zapcore.NewTee(
+		zapcore.NewCore(zapEncoder, zapcore.AddSync(os.Stdout), zapLvl),
+		zapcore.NewCore(zapEncoder, zapcore.AddSync(logFile), zapLvl),
+	)
+
+	zapLogger := zap.New(core, zap.AddCaller(), zap.AddCallerSkip(2))
+
+	return &Logger{
+		Logger: zapLogger,
+		file:   logFile,
+	}, nil
+}
+
+func (l *Logger) Debug(msg string, args ...logger.Field) {
+	l.log(msg, zap.DebugLevel, args...)
+}
+func (l *Logger) Info(msg string, args ...logger.Field) {
+	l.log(msg, zap.InfoLevel, args...)
+}
+func (l *Logger) Warn(msg string, args ...logger.Field) {
+	l.log(msg, zap.WarnLevel, args...)
+}
+func (l *Logger) Error(msg string, args ...logger.Field) {
+	l.log(msg, zap.ErrorLevel, args...)
+}
+
+func (l *Logger) With(field ...zap.Field) *Logger {
+	return &Logger{
+		Logger: l.Logger.With(field...),
+		file:   l.file,
+	}
+}
+
+func (l *Logger) Close() {
+	if err := l.file.Close(); err != nil {
+		fmt.Println("failed to close application logger:", err)
+	}
+}
+
+func (l *Logger) log(msg string, level zapcore.Level, fields ...logger.Field) {
+	if ce := l.Logger.Check(level, msg); ce != nil {
+		zfields := make([]zap.Field, 0, len(fields))
+
+		for _, f := range fields {
+			zfields = append(zfields, zap.Any(f.Key, f.Value))
+		}
+
+		ce.Write(zfields...)
+	}
+}
